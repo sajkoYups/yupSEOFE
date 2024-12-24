86b29fb6af44646995c8964c4391cab8620b6087
sajkoYups <[stefan.sajkovski@yups.tech]>
2024-12-24 14:56:48 +0100

    Add new url for api call

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // export const API_URL =
 //   process.env.REACT_APP_API_URL || "https://yupseonode.onrender.com";
 export const API_URL = "https://yupseonode.onrender.com";
+// export const API_URL = "http://localhost:5001";
